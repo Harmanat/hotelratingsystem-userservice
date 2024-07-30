8324fbb2c199e2aa33bd1f84a225091152ac6319
HarmanatS <wish.mannat@gmail.com>
2024-07-30 15:06:06 +0530

    Implemented FeignClientInterceptor and RestTemplateInterceptor

--- src/main/java/com/mannat/userservice/config/Config.java
+++ src/main/java/com/mannat/userservice/config/Config.java
@@ -1,13 +1,30 @@
 package com.mannat.userservice.config;
 
 //import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import com.mannat.userservice.config.interceptor.RestTemplateInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class Config {
+
+	@Autowired
+	private ClientRegistrationRepository clientRegistrationRepository;
+	@Autowired
+	private OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository;
 
 	/*
 		@Bean: This is used as and when we auto-wire RestTemplate, then no bean is found by Spring Boot, this is how we create bean.
@@ -16,6 +33,35 @@ public class Config {
 	@Bean
 	@LoadBalanced
 	public RestTemplate restTemplate() {
-		return new RestTemplate();
+		RestTemplate restTemplate = new RestTemplate();
+
+		List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+		interceptors.add(new RestTemplateInterceptor(manager(
+				clientRegistrationRepository,
+				oAuth2AuthorizedClientRepository
+		)));
+
+		restTemplate.setInterceptors(interceptors);
+
+		return restTemplate;
+	}
+
+	// declare the bean of OAuth2AuthorizedClient Manager
+	@Bean
+	public OAuth2AuthorizedClientManager manager(
+			ClientRegistrationRepository clientRegistrationRepository,
+			OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository
+	){
+		OAuth2AuthorizedClientProvider provider = OAuth2AuthorizedClientProviderBuilder.builder()
+				.clientCredentials().build();
+
+		DefaultOAuth2AuthorizedClientManager defaultOAuth2AuthorizedClientManager = new
+				DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository, oAuth2AuthorizedClientRepository);
+
+		defaultOAuth2AuthorizedClientManager.setAuthorizedClientProvider(provider);
+
+		return defaultOAuth2AuthorizedClientManager;
+
+
 	}
 }
---
+++ src/main/java/com/mannat/userservice/config/interceptor/FeignClientInterceptor.java
@@ -0,0 +1,35 @@
+package com.mannat.userservice.config.interceptor;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.stereotype.Component;
+
+
+@Configuration
+@Component
+public class FeignClientInterceptor implements RequestInterceptor {
+
+    @Autowired
+    private OAuth2AuthorizedClientManager manager;
+
+    @Override
+    public void apply(RequestTemplate template) {
+
+        String token = manager
+                        .authorize(OAuth2AuthorizeRequest.withClientRegistrationId("my-internal-client")
+                        .principal("internal").build())
+                        .getAccessToken()
+                        .getTokenValue();
+
+        template.header("Authorization", "Bearer "+token);
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    }
+}
